1ad896ee0fb2926f44f971aaf78acc670c37faef
alpha1638888 <167107906+alpha1638888@users.noreply.github.com>
2024-12-25 14:25:33 +0800

    Update genJsc.js

--- genJsc.js
+++ genJsc.js
@@ -1,3 +1,2 @@
-const os = require("os");
 const bytenode = require("bytenode");
-bytenode.compileFile({ filename: './dist/Test.js', output: './dist/Test_' + os.platform() + '.jsc' });
+bytenode.compileFile({ filename: './dist/Test.js', output: './dist/Test_' + process.platform + '_' + process.versions.node.split('.')[0] + '.jsc' });
